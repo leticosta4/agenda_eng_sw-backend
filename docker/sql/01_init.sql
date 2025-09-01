faa980e150038267baa9117cb699e22244864365
VerttB <alyssonoliveira456@gmail.com>
2025-09-01 09:58:12 -0300

    fix: Fix sql create tables

--- docker/sql/01_init.sql
+++ docker/sql/01_init.sql
@@ -1,16 +1,18 @@
-CREATE EXTENSION IF NOT EXISTIS "uuid-ossp"
 
-CREATE TABLE IF NOT EXISTS contato{
-    id UUID  NOT NULL generate_random_uuid(),
-    agenda_id UUID NOT NULL
-    nome VARCHAR(50) NOT NULL,
-    telefone VARCHAR(11) NOT NULL,
-    PRIMARY KEY("id")
-    CONSTRAINT "FK_agenda_id" FOREIGN KEY ("agenda_id") REFERENCES "agenda"("id") ON UPDATE CASCADE ON DELETE,
-}
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTIS agenda{
-    id UUID NOT NULL generate_random_uuid(),
-    nome VARCHAR(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS agenda (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "nome" VARCHAR(50) NOT NULL,
     PRIMARY KEY("id")
-}
+);
+
+CREATE TABLE IF NOT EXISTS contato (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "agenda_id" UUID DEFAULT NULL,
+    "nome" VARCHAR(50) NOT NULL,
+    "telefone" VARCHAR(11) NOT NULL,
+    PRIMARY KEY("id"),
+    CONSTRAINT "FK_agenda_id" FOREIGN KEY ("agenda_id") REFERENCES "agenda"("id") ON UPDATE CASCADE ON DELETE SET NULL
+);
+
