21f518d225f87d3eea12111071746ed71900ab91
let <leticiacostaoa@gmail.com>
2025-09-02 23:28:48 -0300

    refactor: remove unnecessary annotation use and import

--- src/main/java/com/engsw/agenda/AgendaApplication.java
+++ src/main/java/com/engsw/agenda/AgendaApplication.java
@@ -2,10 +2,8 @@ package com.engsw.agenda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application-secrets.properties")
 public class AgendaApplication {
 
 	public static void main(String[] args) {
