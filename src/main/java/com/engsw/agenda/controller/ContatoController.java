ee41be52d8d2d98d675a9244f3735927f87ac9b1
Verttb <alyssonoliveirai456@gmail.com>
2025-09-13 01:33:42 -0300

    feat: CRUD para contatos

---
+++ src/main/java/com/engsw/agenda/controller/ContatoController.java
@@ -0,0 +1,58 @@
+package com.engsw.agenda.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.engsw.agenda.dto.contato.ContatoDTO;
+import com.engsw.agenda.dto.contato.ContatoRespostaDTO;
+import com.engsw.agenda.repository.ContatoRepository;
+import com.engsw.agenda.service.ContatoService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/contatos")
+public class ContatoController {
+    @Autowired private ContatoService contatoService;
+
+
+
+    @GetMapping
+    public ResponseEntity<List<ContatoRespostaDTO>> listarContatos(){
+        List<ContatoRespostaDTO> contatos = contatoService.buscarContatos();
+        return ResponseEntity.ok(contatos);
+    }
+
+    @GetMapping("/{contatoId}")
+    public ResponseEntity<ContatoRespostaDTO> buscarPorId(@PathVariable UUID contatoId){
+        return contatoService.buscarContatoPorId(contatoId)
+               .map(ResponseEntity::ok)
+               .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{contatoId}")
+    public ResponseEntity<ContatoRespostaDTO> editarContato(@PathVariable UUID contatoId, @Valid @RequestBody ContatoDTO contatoNovo){
+        System.out.println("Nome" + contatoNovo.getNome() + "TELEFONE:" + contatoNovo.getTelefone());
+        ContatoRespostaDTO contatoEditado = contatoService.editarContato(contatoId, contatoNovo);
+        return ResponseEntity.ok(contatoEditado);
+    }
+
+    @DeleteMapping("/{contatoId}")
+    public ResponseEntity<Void> excluirContato(@PathVariable UUID contatoId){
+        contatoService.excluirContato(contatoId);
+
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
--- src/main/java/com/engsw/agenda/service/ContatoService.java
+++ src/main/java/com/engsw/agenda/service/ContatoService.java
@@ -1,5 +1,66 @@
 package com.engsw.agenda.service;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.hibernate.type.descriptor.java.LocalDateTimeJavaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.engsw.agenda.dto.contato.ContatoDTO;
+import com.engsw.agenda.dto.contato.ContatoRespostaDTO;
+import com.engsw.agenda.model.Contato;
+import com.engsw.agenda.repository.ContatoRepository;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+
+@Service
 public class ContatoService {
-    
+    @Autowired private ContatoRepository contatoRepository;
+    public List<ContatoRespostaDTO> buscarContatos(){
+        List<Contato> contatos = contatoRepository.findAll();
+
+        return contatos.stream().map(ContatoRespostaDTO::new).collect(Collectors.toList());
+    }
+
+    public Optional<ContatoRespostaDTO> buscarContatoPorId(UUID contatoId){
+        Optional<Contato> contato = contatoRepository.findById(contatoId);
+
+        return contato.map(ContatoRespostaDTO::new);
+    }
+
+    public ContatoRespostaDTO editarContato(UUID contatoId, ContatoDTO contatoNovo){
+        Contato contato = 
+                        contatoRepository.findById(contatoId)
+                        .orElseThrow(() -> new EntityNotFoundException("Contato não encontrado"));
+
+                        
+        if (contatoNovo.getNome() != null) {
+            contato.setNome(contatoNovo.getNome());
+        }
+        if (contatoNovo.getTelefone() != null) {
+            contato.setTelefone(contatoNovo.getTelefone());
+        }
+        contato.setModificadoEm(LocalDateTime.now());
+        contatoRepository.save(contato);
+        return new ContatoRespostaDTO(contato);
+    }
+
+    public void excluirContato(UUID contatoId){
+        if(!contatoRepository.existsById(contatoId)){
+            throw new EntityNotFoundException("Contato não Encontrado");
+        };
+
+        contatoRepository.deleteById(contatoId);
+
+    }
+
 }
