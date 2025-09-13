e1c64fe2cb5a8b286b1f52c92f1be43d3f529d83
let <leticiacostaoa@gmail.com>
2025-09-13 00:19:29 -0300

    adicionando mais da modelagem combinada

---
+++ src/main/java/com/engsw/agenda/service/agenda/FabricaAgenda.java
@@ -0,0 +1,22 @@
+package com.engsw.agenda.service.agenda;
+
+//falta refinar ainda
+public class FabricaAgenda {
+    public static final int AGENDAMAP = 0;
+    public static final int AGENDALIST = 1;
+
+    private static final FabricaAgenda fabricaAgenda = new FabricaAgenda();
+
+    public static FabricaAgenda getInstancia(){
+        return null;
+    }
+
+    private FabricaAgenda(){}
+
+    public IAgenda criarAgenda(int tipo){
+        return null;
+    }
+
+}
+
+//melhorar esse singleton com factory
