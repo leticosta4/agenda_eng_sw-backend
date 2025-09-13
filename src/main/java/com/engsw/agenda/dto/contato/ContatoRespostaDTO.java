5527932b753707f38a08422f2175609e8f388ce8
Verttb <alyssonoliveirai456@gmail.com>
2025-09-13 01:33:15 -0300

    feat: DTO para contato

---
+++ src/main/java/com/engsw/agenda/dto/contato/ContatoRespostaDTO.java
@@ -0,0 +1,32 @@
+package com.engsw.agenda.dto.contato;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.engsw.agenda.model.Agenda;
+import com.engsw.agenda.model.Contato;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ContatoRespostaDTO {
+    private UUID id;
+    private String agenda;
+    private String nome;
+    private String telefone;
+    private LocalDateTime criadoEm;
+    private LocalDateTime modificadoEm;
+
+    public ContatoRespostaDTO(Contato c){
+        this.id = c.getId();
+        this.nome = c.getNome();
+        this.agenda = c.getAgenda().getNome();
+        this.telefone = c.getTelefone();
+        this.criadoEm = c.getCriadoEm();
+        this.modificadoEm = c.getModificadoEm();
+
+    }
+}
